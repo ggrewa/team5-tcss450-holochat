bc7f513a18aa4d0a2d3a3baeaa3d013c2232a1a7
Ken Ahrens <75004340+Kenpai718@users.noreply.github.com>
2022-05-18 20:25:37 -0700

    setting up contacts endpoint

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ app.use('/service', require('./routes/verification.js'))
 app.use('/changePassword', require('./routes/sendVerifCode.js'))
 
 app.use('/changePassword', require('./routes/updatePassword.js'))
+
+//app.use('/contacts', middleware.checkToken,  require('./routes/contacts.js'))
+app.use('/contacts', require('./routes/contacts.js'))
 /*
  * Return HTML for the / end point. 
  * This is a nice location to document your web service API
