8d998a0185a416570e49d951f7ec0248a7893ffc
Ken Ahrens <75004340+Kenpai718@users.noreply.github.com>
2022-06-02 19:53:08 -0700

    route to get outgoing friend request

--- routes/contacts.js
+++ routes/contacts.js
@@ -78,6 +78,76 @@ router.get("/", (request, response, next) => {
 });
 
 /**
+ * @api {get} /contacts Request to get list of outgoing contact requests
+ * @apiName GetContacts
+ * @apiGroup Contacts
+ * 
+ * @apiDescription Request to get list of contacts
+ * 
+ * @apiSuccess {Object[]} contacts List of contacts
+ * @apiSuccess {boolean} success true on successful SQL query
+ * @apiSuccess {String} email the email of the current user
+ * 
+ * @apiError (404: memberId Not Found) {String} message "member ID Not Found"
+ * 
+ * @apiError (400: SQL Error) {String} message the reported SQL error details
+ * 
+ * @apiUse JSONError
+ */
+ router.get("/outgoing", (request, response, next) => {
+    console.log("/contacts/outgoing");
+    // console.log("User token member id: " + request.decoded.memberid);
+    if (!request.decoded.memberid) {
+        response.status(400).send({
+            message: "Missing required information"
+        })
+    } else if (isNaN(request.decoded.memberid)) {
+        response.status(400).send({
+            message: "Malformed parameter. memberId must be a number"
+        })
+    } else {
+        next()
+    }
+}, (request, response) => {
+    //Get contact info
+    let query = 'SELECT Verified, MemberID_A, Members.FirstName, Members.LastName, Members.email, Members.Username FROM Contacts INNER JOIN Members ON Contacts.MemberID_A = Members.MemberID where Contacts.MemberID_B = $1'
+    let values = [request.decoded.memberid]
+
+    pool.query(query, values)
+        .then(result => {
+            if (result.rowCount == 0) {
+                response.status(404).send({
+                    message: "Contact not found"
+                })
+            } else {
+                let listContacts = [];
+                result.rows.forEach(entry =>
+                    listContacts.push(
+                        {
+                            "email": entry.email,
+                            "firstName": entry.firstname,
+                            "lastName": entry.lastname,
+                            "userName": entry.username,
+                            "memberId": entry.memberid_b,
+                            "verified": entry.verified
+                        }
+                    )
+                )
+                response.send({
+                    success: true,
+                    email: request.decoded.email,
+                    contacts: listContacts
+                })
+            }
+        }).catch(error => {
+            response.status(400).send({
+                message: "SQL Error",
+                error: error
+            })
+        })
+});
+
+/**
  * @api {get} /contacts/search Request for a search of contacts based on input string. 
  * @apiName SearchContacts
  * @apiGroup Contacts
